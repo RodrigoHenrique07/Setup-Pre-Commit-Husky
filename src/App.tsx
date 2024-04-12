351fa179673300a1e9153ec46666a25f03bcceec
RodrigoHenrique <rodrigo_pbro@hotmail.com>
2024-04-11 23:55:45 -0300

    chore: add setup de pre-commit utilizando husky

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <>
+            <h1>
+                Hellow Setup pré-commit utilizando husky, lint-staged e
+                commintlint
+            </h1>
+        </>
+    )
+}
+
+export default App
